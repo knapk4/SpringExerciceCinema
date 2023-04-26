d7fac96914d84cc235ac19fa2be8c3861e2f610a
pierrega-semifir <pierre.gaillard@semifir.com>
2023-04-26 11:43:12 +0200

    Premier commit du projet, ajout des fichiers de base

---
+++ src/test/java/fr/piga/cinemaspring/CinemaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.piga.cinemaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
